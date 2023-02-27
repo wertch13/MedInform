6252c4f035f1b465b29c4087f3f8ae462d964cf2
Zanin Artyom <115012938+wertch13@users.noreply.github.com>
2023-02-27 15:09:43 +0300

    Add files via upload

---
+++ JS_01.js
@@ -0,0 +1,21 @@
+'Привет, Java'
+var myThing;
+myThing = 13;
+myThing = "это строка";
+var numberNine = 9;
+var stringNine = "9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = 'Привет,';
+var myName = 'Артём';
+greeting + myName;
+myName.length;
+
+name_orz = 'ОРЗ ';
+name_vetr = ', Ветрянка ';
+num_orz_mon = 2;
+num_orz_tues = 3;
+num_vetr_tues = 1;
+num_orz_wedn = 2;
+num_vetr_wedn = 4;
+num_orz_week = name_orz + (num_orz_mon  + num_orz_tues + num_orz_wedn) + name_vetr + (num_vetr_tues + num_vetr_wedn);
